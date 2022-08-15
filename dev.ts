57124ff0acbd138edcf0b1b9f5bc6cc4c0a69169
Lê Ngọc Trường (Tommy) <wolfcancode@gmail.com>
2022-08-16 02:09:46 +0700

    fix: config to export all

--- dev.ts
+++ dev.ts
@@ -1,4 +1,5 @@
-#!/usr/bin/env -S deno run -A --watch=static/,routes/
+#!/usr/bin/env -S deno run -A --watch=static/,routes/ --allow-env --allow-read 
+import "https://deno.land/x/dotenv/load.ts";
 
 import dev from "$fresh/dev.ts";
 
