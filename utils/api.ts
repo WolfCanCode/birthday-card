4641c2d2fb7051812497a83e17fb8893721dac8d
Lê Ngọc Trường (Tommy) <wolfcancode@gmail.com>
2022-08-16 02:11:50 +0700

    fix: env

--- utils/api.ts
+++ utils/api.ts
@@ -1,9 +1,8 @@
 import { supabaseClient,supabaseTableItems } from "https://deno.land/x/supabase_deno/mod.ts";
-import { ENV } from "./env.ts";
 
 export const sb = new supabaseClient(
-  ENV.SUPABASE_URL,
-  ENV.SUPABASE_KEY,
+  Deno.env.get("SUPABASE_URL") || "",
+  Deno.env.get("SUPABASE_KEY") || "",
 );
 
 export const sbInivations = new supabaseTableItems(sb, "invitations");
