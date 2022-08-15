fe1bb76c63e9ccdd5c48f69f825481ca09395fbd
Lê Ngọc Trường (Tommy) <wolfcancode@gmail.com>
2022-08-16 02:04:17 +0700

    fix: config to export all

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,9 @@
 import { supabaseClient,supabaseTableItems } from "https://deno.land/x/supabase_deno/mod.ts";
+import { ENV } from "./env.ts";
+
 export const sb = new supabaseClient(
-  Deno.env.get("SUPABASE_URL") || "",
-  Deno.env.get("SUPABASE_KEY") || "",
+  ENV.SUPABASE_URL,
+  ENV.SUPABASE_KEY,
 );
 
 export const sbInivations = new supabaseTableItems(sb, "invitations");
