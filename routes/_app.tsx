7abe56dc2b030e91fabe476ee2982c0ebe6f9e4c
Lê Ngọc Trường (Tommy) <wolfcancode@gmail.com>
2022-08-29 12:02:14 +0700

    feat: update seo

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,3 @@
-
 /** @jsx h */
 /** @jsxFrag Fragment */
 import { Fragment, h } from "preact";
@@ -10,6 +9,23 @@ export default function App({ Component }: AppProps) {
     <>
       <Head>
         <title>Ngọt Birthday</title>
+        <meta
+          property="og:url"
+          content="https://sweetbirthday.deno.dev/"
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content="Ngọt said, I am one year old"
+        />
+        <meta
+          property="og:description"
+          content="Đây là thiệp sinh nhật điện tử, có giá trị để Ngọt biết bạn có đi hay không. Ngoài ra nó cũng là vé để bốc thăm trúng thưởng đó nha"
+        />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/58ffyC0/avatar.png"
+        />
       </Head>
       <Component />
     </>
